e2ed13c3714c7da2c767a65ac02aeb13cc290587
BrandonL02 <89661981+BrandonL02@users.noreply.github.com>
2023-01-12 10:37:28 -0500

    Add files via upload

---
+++ EVO Note Center/suggestions.js
@@ -0,0 +1,10 @@
+let suggestions = [
+	"Case Notes",
+    "Notes",
+    "Rekeys",
+    "Add Terminal",
+    "Add Terminal (Multi-Merchant)",
+    "Change Terminal",
+    "Change Datacap File",
+    "Rekey add terminal"
+];
